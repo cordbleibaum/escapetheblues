1259d1a955aa112a0128752a470e31c4d0e6eb67
cordbleibaum <cordbleibaum@icloud.com>
2018-10-13 01:12:16 +0200

    goal position generation

--- Assets/Scripts/Logic/Character.cs
+++ Assets/Scripts/Logic/Character.cs
@@ -4,5 +4,27 @@ public class Character : MonoBehaviour
 {
     public CharacterGroup group;
     
-    public readonly Vector2Int personalGoalPosition;
+    public Vector2Int personalGoalPosition;
+
+    private void Start()
+    {
+        int goalX = Random.Range(0, group.world.width);
+        int goalY = Random.Range(0, group.world.height);
+        if (goalY == 0 && goalX == 0)
+        {
+            while (goalX == 0)
+            {
+                goalX = Random.Range(0, group.world.width);
+            }
+        }
+
+        if (goalX == group.world.width - 1 && goalY == personalGoalPosition.y)
+        {
+            while (goalY == personalGoalPosition.y)
+            {
+                goalY = Random.Range(0, group.world.height);
+            }
+        }
+        personalGoalPosition = new Vector2Int(goalX, goalY);
+    }
 }
--- Assets/Scripts/Logic/CharacterGroup.cs
+++ Assets/Scripts/Logic/CharacterGroup.cs
@@ -5,5 +5,12 @@ public class CharacterGroup : MonoBehaviour
 	public World world;
 	
 	public Character[] characters;
-	public readonly Vector2Int goalPosition;
+	public Vector2Int goalPosition;
+
+	private void Start()
+	{
+		int goalX = world.width - 1;
+		int goalY = Random.Range(0, world.height);
+		goalPosition = new Vector2Int(goalX, goalY);
+	}
 }
