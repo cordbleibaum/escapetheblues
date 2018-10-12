a3e8b850ec40c726394420890fdbc6672cc06fa5
cordbleibaum <cordbleibaum@icloud.com>
2018-10-13 00:50:56 +0200

    some more data I know now

--- Assets/Scripts/Logic/Character.cs
+++ Assets/Scripts/Logic/Character.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/Logic/World.cs
+++ Assets/Scripts/Logic/World.cs
@@ -6,9 +6,12 @@ public class World : MonoBehaviour
 {
     public int startSadness;
     public int maxSadness;
+    
+    public int width = 8;
+    public int height = 8;
 
     private int sadness;
-
+    
     private void Start()
     {
         sadness = startSadness;
