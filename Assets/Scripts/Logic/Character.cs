2a6495f76d14a224a684939afb5c82f9c5d3a5a4
cordbleibaum <cordbleibaum@icloud.com>
2018-10-13 01:29:39 +0200

    conventions and create update on tile

--- Assets/Scripts/Logic/Character.cs
+++ Assets/Scripts/Logic/Character.cs
@@ -4,30 +4,30 @@ namespace Logic
 {
     public class Character : MonoBehaviour
     {
-        public CharacterGroup group;
+        public CharacterGroup Group;
     
-        public Vector2Int personalGoalPosition;
+        public Vector2Int PersonalGoalPosition;
 
         private void Start()
         {
-            var goalX = Random.Range(0, group.World.Width);
-            var goalY = Random.Range(0, group.World.Height);
+            var goalX = Random.Range(0, Group.World.Width);
+            var goalY = Random.Range(0, Group.World.Height);
             if (goalY == 0 && goalX == 0)
             {
                 while (goalX == 0)
                 {
-                    goalX = Random.Range(0, group.World.Width);
+                    goalX = Random.Range(0, Group.World.Width);
                 }
             }
 
-            if (goalX == group.World.Width - 1 && goalY == personalGoalPosition.y)
+            if (goalX == Group.World.Width - 1 && goalY == PersonalGoalPosition.y)
             {
-                while (goalY == personalGoalPosition.y)
+                while (goalY == PersonalGoalPosition.y)
                 {
-                    goalY = Random.Range(0, group.World.Height);
+                    goalY = Random.Range(0, Group.World.Height);
                 }
             }
-            personalGoalPosition = new Vector2Int(goalX, goalY);
+            PersonalGoalPosition = new Vector2Int(goalX, goalY);
         }
     }
 }
--- Assets/Scripts/Presentation/Tile.cs
+++ Assets/Scripts/Presentation/Tile.cs
@@ -17,5 +17,10 @@ namespace Presentation
         {
 
         }
+
+        private void Update()
+        {
+            
+        }
     }
 }
