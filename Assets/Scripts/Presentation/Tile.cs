2b46cbb580c699b1e9e9efb615190f46fa7f4332
cordbleibaum <cordbleibaum@icloud.com>
2018-10-13 00:56:20 +0200

    skeleton for tile

--- Assets/Scripts/Presentation/Tile.cs
+++ Assets/Scripts/Presentation/Tile.cs
@@ -1,5 +1,13 @@
 ﻿using UnityEngine;
 
 public class Tile : MonoBehaviour {
+    public enum Direction
+    {
+        Left, Right, Up, Down
+    }
 
+    public void transition(Direction direction)
+    {
+        
+    }
 }
