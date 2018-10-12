b7a30ca9a72c6c2f918e021b458cd829d1a2ac71
cordbleibaum <cordbleibaum@icloud.com>
2018-10-12 22:32:09 +0200

    gitignore, base world object and line endings

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public int startSadness;
+    public int maxSadness;
+
+    private int sadness;
+
+    private void Start()
+    {
+        sadness = startSadness;
+    }
+}
