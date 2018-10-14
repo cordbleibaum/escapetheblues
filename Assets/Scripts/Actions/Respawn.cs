b6e78bae12b5b6b079257e46a7bdbbdf0a6fdede
cordbleibaum <cordbleibaum@icloud.com>
2018-10-14 15:13:28 +0200

    respawn

---
+++ Assets/Scripts/Actions/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour {
+	private void Start ()
+	{
+		StartCoroutine(RespawnDelay());
+	}
+	
+	private IEnumerator RespawnDelay()
+	{
+		yield return new WaitForSeconds(4);
+		SceneManager.LoadScene(1);
+	}
+}
