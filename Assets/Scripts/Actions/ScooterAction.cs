d2929075d8cba670bae2f43944eb8d31eba4825a
cordbleibaum <cordbleibaum@icloud.com>
2018-10-14 10:04:07 +0200

    autoscooter

---
+++ Assets/Scripts/Actions/ScooterAction.cs
@@ -0,0 +1,11 @@
+﻿using Logic;
+using UnityEngine;
+
+public class ScooterAction : MonoBehaviour {
+	private void OnEnable()
+	{
+		CharacterGroup.instance.grandpa.goalFulfilled = true;
+		CharacterGroup.instance.timmy.sadness += 10;
+		CharacterGroup.instance.uncle.sadness -= 10;
+	}
+}
