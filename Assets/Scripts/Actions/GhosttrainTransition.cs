447e9a9ddc46161f5f5151cdb3eb31d811c4e9f9
cordbleibaum <cordbleibaum@icloud.com>
2018-10-14 15:06:36 +0200

    win condition

--- Assets/Scripts/Actions/GhosttrainTransition.cs
+++ Assets/Scripts/Actions/GhosttrainTransition.cs
@@ -1,9 +1,14 @@
+using Logic;
+using Presentation;
 using UnityEngine;
 
 namespace Actions
 {
     public class GhosttrainTransition : MonoBehaviour
     {
-        
+        private void OnEnable()
+        {
+            World.instance.Teleport(RollercoasterPosition.position.x, RollercoasterPosition.position.y);
+        }
     }
 }
